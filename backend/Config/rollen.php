befe8539bca0c76d502804db6c2e6245b1c87462
xXWapixelXx <144104696+xXWapixelXx@users.noreply.github.com>
2026-01-28 13:44:09 +0100

    Add rollen.php with role ID to name mapping
    Introduces a new configuration file defining a mapping between role IDs and their corresponding role names for the project.

---
+++ backend/Config/rollen.php
@@ -0,0 +1,14 @@
+<?php
+// Naam: Wail Said, Aaron Verdoold, Anwar Azarkan, Dylan Versluis
+// Project: Kringloop Centrum Duurzaam
+// Datum: 28-01-2026
+// Beschrijving: Overzicht van rol_id waarden en bijbehorende rollen
+
+// Simpele mapping van rol_id naar rolnaam
+$ROL_LEN = [
+    1 => 'Directie',
+    2 => 'Winkelpersoneel',
+    3 => 'Magazijnmedewerker',
+    4 => 'Chauffeur',
+];
+
