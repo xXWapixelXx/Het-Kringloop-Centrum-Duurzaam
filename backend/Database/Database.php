69953475224f667e5ac52465a0630ddfe58de304
xXWapixelXx <144104696+xXWapixelXx@users.noreply.github.com>
2026-01-29 12:26:18 +0100

    Update Database.php

--- backend/Database/Database.php
+++ backend/Database/Database.php
@@ -2,18 +2,19 @@
 // Naam: Wail Said, Aaron Verdoold, Anwar Azarkan, Dylan Versluis
 // Versie: 1.0
 // Datum: 28-01-2026
-// Beschrijving: Databaseverbinding via PDO voor project duurzaam
+// Beschrijving: Databaseverbinding via PDO. connect() is protected zodat alleen classes die extends Database doen (onze DAO’s) de verbinding kunnen gebruiken.
 
 declare(strict_types=1);
 
 class Database
 {
+    // Alleen aanroepbaar door deze class en door DAO’s (GebruikerDAO, ArtikelDAO, etc.) die extends Database doen
     protected function connect()
     {
         $servername = "localhost";
         $username = "root";
         $password = "";
-        $port = 3307;
+        $port = 3306;
         $database = "duurzaam";
 
         try {
